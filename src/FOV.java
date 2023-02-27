4a136eef0acc892fca6262a7f6889276a82be36d
SethV <seth.vizel@gmail.com>
2023-02-27 07:30:28 -0500

    Implementing FOV Try Implementing A*?

--- src/FOV.java
+++ src/FOV.java
@@ -5,44 +5,46 @@ public class FOV {
     private final Player player;
     private final ArrayList<Enemy> enemies;
     private final int viewRange;
-    private MazeItem[][] FOV;
+    private final int[] frame;
+    private final MazeItem[][] FOV;
 
     public FOV(Tile[][] worldMap, Player player, ArrayList<Enemy> enemies, int viewRange) {
         this.worldMap = worldMap;
         this.player = player;
         this.enemies = enemies;
         this.viewRange = viewRange;
+        frame = new int[4];
+        FOV = new MazeItem[viewRange][viewRange];
     }
 
-    private int[] getFOVFrame(int viewRange){
+    private boolean withinFrame(int x, int y) {
+        return (frame[0] <= x && x < frame[1]) && (frame[2] <= y && y < frame[3]);
+    }
+
+    private void focusFrame(){
         int x = player.x(), y = player.y();
         int r = viewRange/2;
 
-        int xStart = Math.max(x - r, 0) - (x + r >= worldMap.length ? r + x - worldMap.length + 1: 0);
-        int xEnd = Math.min(x + r, worldMap.length - 1) + (x - r < 0 ? r - x : 0);
-        int yStart = Math.max(y - r, 0) - (y + r >= worldMap.length ? r + y - worldMap.length + 1: 0);
-        int yEnd = Math.min(y + r, worldMap.length - 1) + (y - r < 0 ? r - y : 0);
-
-        return new int[]{xStart, xEnd, yStart, yEnd};
+        frame[0] = Math.max(x - r, 0) - (x + r >= worldMap.length ? r + x - worldMap.length + 1: 0);
+        frame[1] = Math.min(x + r, worldMap.length - 1) + (x - r < 0 ? r - x : 0);
+        frame[2] = Math.max(y - r, 0) - (y + r >= worldMap.length ? r + y - worldMap.length + 1: 0);
+        frame[3] = Math.min(y + r, worldMap.length - 1) + (y - r < 0 ? r - y : 0);
     }
 
-    public void focus() {
-        MazeItem[][] FOV = new MazeItem[viewRange][viewRange];
+    private void focus() {
+        focusFrame();
 
         for (int yi = frame[2]; yi <= frame[3]; yi++) {
-            for (int xi = frame[0]; xi <= frame[1]; xi++) {
-                FOV[yi - frame[2]][xi - frame[0]] = worldMap[yi][xi];
-            }
+            if (frame[1] + 1 - frame[0] >= 0)
+                System.arraycopy(worldMap[yi], frame[0], FOV[yi - frame[2]], 0, frame[1] + 1 - frame[0]);
         }
 
         for (Enemy enemy : enemies) {
-            if ((frame[0] <= enemy.x() && enemy.x() < frame[1]) && (frame[2] <= enemy.y() && enemy.y() < frame[3])){
+            if (withinFrame(enemy.x(), enemy.y())){
                 FOV[enemy.y() - frame[2]][enemy.x() - frame[0]] = enemy;
             }
         }
 
         FOV[player.y() - frame[2]][player.x() - frame[0]] = player;
-
-        this.FOV = FOV;
     }
 }
